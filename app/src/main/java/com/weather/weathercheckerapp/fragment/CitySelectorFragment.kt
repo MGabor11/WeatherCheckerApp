03157ed75b78954f0b41240c1c2ad0340ca88620
Eszter Judit Tóth <Eszter.Toth@betssongroup.com>
2018-09-28 15:52:42 +0200

    added google places, added search fragment to CitySelectorFragment

--- app/src/main/java/com/weather/weathercheckerapp/fragment/CitySelectorFragment.kt
+++ app/src/main/java/com/weather/weathercheckerapp/fragment/CitySelectorFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment
 import com.weather.weathercheckerapp.R
 
 class CitySelectorFragment : Fragment(){
 
+    val autocompleteFragment = fragmentManager?.findFragmentById(R.id.place_autocomplete_fragment) as PlaceAutocompleteFragment?
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_city_selector, container, false)
+
     }
 
 }
