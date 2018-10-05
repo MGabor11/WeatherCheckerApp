4c4dbc9efc8da7b02923759f08ee9d6a459ebd78
Eszter Judit Tóth <Eszter.Toth@betssongroup.com>
2018-10-05 10:45:00 +0200

    refactor

--- app/src/main/java/com/weather/weathercheckerapp/fragment/CitySelectorFragment.kt
+++ app/src/main/java/com/weather/weathercheckerapp/fragment/CitySelectorFragment.kt
@@ -2,17 +2,15 @@ package com.weather.weathercheckerapp.fragment
 
 import android.content.Context
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.lifecycle.ViewModelProviders
-import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment
 import com.weather.weathercheckerapp.R
 import com.weather.weathercheckerapp.ViewModelFactory
+import com.weather.weathercheckerapp.util.onChange
 import com.weather.weathercheckerapp.viewmodel.CitySelectorViewModel
 import dagger.android.support.AndroidSupportInjection
 import dagger.android.support.DaggerFragment
@@ -24,15 +22,7 @@ class CitySelectorFragment : DaggerFragment(){
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
 
-    lateinit var queryCity : String
-
     lateinit var viewModel: CitySelectorViewModel
-
-    lateinit var placeAutocompleteInput : EditText
-
-    lateinit var searchIcon : ImageView
-
-    lateinit var clearIcon : ImageView
 
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
@@ -45,32 +35,25 @@ class CitySelectorFragment : DaggerFragment(){
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = inflater.inflate(R.layout.fragment_city_selector, container, false)
-        initAutoCompleteInputStyle(view)
+        val view = inflater.inflate(R.layout.fragment_city_selector, container, false)
+        initAutoCompleteInput(view)
         return view
     }
 
-    private fun initAutoCompleteInputStyle(view: View) {
-        placeAutocompleteInput = view.findViewById(R.id.place_autocomplete_search_input)
-        searchIcon = view.findViewById(R.id.place_autocomplete_search_button)
-        searchIcon.setImageDrawable(resources.getDrawable(R.drawable.ic_search))
-        clearIcon = view.findViewById(R.id.place_autocomplete_clear_button)
-        clearIcon.setImageDrawable(resources.getDrawable(R.drawable.ic_clear))
-        placeAutocompleteInput.setTextColor(resources.getColor(R.color.primaryTextColor))
-        placeAutocompleteInput.setHintTextColor(resources.getColor(R.color.primaryTextColor))
-        placeAutocompleteInput.onChange { onCitySelected() }
-    }
+    private fun initAutoCompleteInput(view: View) {
+        view.findViewById<ImageView>(R.id.place_autocomplete_search_button).apply {
+            setImageDrawable(resources.getDrawable(R.drawable.ic_search, null))
+        }
 
-    private fun onCitySelected() {
-        queryCity = placeAutocompleteInput.getText().toString()
-    }
+        view.findViewById<ImageView>(R.id.place_autocomplete_clear_button).apply {
+            setImageDrawable(resources.getDrawable(R.drawable.ic_clear, null))
+        }
 
-    private fun EditText.onChange(cb: (String) -> Unit) {
-        this.addTextChangedListener(object: TextWatcher {
-            override fun afterTextChanged(s: Editable?) { cb(s.toString()) }
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-        })
+        view.findViewById<EditText>(R.id.place_autocomplete_search_input).apply {
+            setTextColor(resources.getColor(R.color.primaryTextColor))
+            setHintTextColor(resources.getColor(R.color.primaryTextColor))
+            onChange { viewModel.queryCity = text.toString() }
+        }
     }
 
 }
--- app/src/main/java/com/weather/weathercheckerapp/viewmodel/CitySelectorViewModel.kt
+++ app/src/main/java/com/weather/weathercheckerapp/viewmodel/CitySelectorViewModel.kt
@@ -3,4 +3,8 @@ package com.weather.weathercheckerapp.viewmodel
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
-class CitySelectorViewModel @Inject constructor() : ViewModel()
+class CitySelectorViewModel @Inject constructor() : ViewModel() {
+
+    var queryCity : String? = null
+
+}
